d97bfc29f95758cfbc2a9c1a00f31f30514d0cd4
de-sage <gomotosho1@gmail.com>
2021-05-17 11:39:48 +0100

    added test files

---
+++ src/test/java/secondchapter/ArithmeticTest.java
@@ -0,0 +1,14 @@
+package secondchapter;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArithmeticTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+}
